2f33320b1949edf8c0902268dcff5b3d24736250
parigyanofficial <vectorsteve100@gmail.com>
2026-02-04 10:08:48 +0530

    Create calling_fuction_outisde_class.cpp

---
+++ Access_specifiers/calling_fuction_outisde_class.cpp
@@ -0,0 +1,30 @@
+//calling a function outside class......readabilty
+#include <iostream>
+using namespace std;
+
+class Student{
+    private:
+    int roll;
+    string name;
+    public:
+    void getdata();
+    void display();
+};
+
+void Student::getdata()
+{
+    cout<<"Enter rollno,name";
+    cin>>roll>>name;
+}
+void Student::display()
+{
+    getdata();
+    cout<<"Rollno:"<<roll<<endl;
+    cout<<"Name:"<<name;
+}
+
+int main(){
+    Student s1;
+    s1.display();
+    return 0;
+}
