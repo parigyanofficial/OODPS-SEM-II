0ae74fddcc0a42fc61f92dcb2f8676a5955a1feb
parigyanofficial <vectorsteve100@gmail.com>
2026-02-04 10:58:35 +0530

    Create Online_shopping_product.cpp

---
+++ Access_specifiers/Online_shopping_product.cpp
@@ -0,0 +1,53 @@
+/*
+Online shoppign product 
+An e commerce system stores product details;
+
+Class: Product
+Data Members: productId, productName, price, stock
+Tasks:  Create the class
+        Creat two objects 
+        display details of both products
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+class Product{
+    private:
+    string productId, productName;
+    int price, stock;
+    public:
+    void inputDetail();
+    void showDetail();
+};
+
+void Product::inputDetail(){
+    cout<<"productId, productName, price, stock";
+    cin>>productId>>productName>>price>>stock;
+}
+
+void Product::showDetail(){
+    cout<<"\nProduct ID:"<<productId<<endl;
+    cout<<"Product Name:"<<productName<<endl;
+    cout<<"Price:"<<price<<endl;
+    cout<<"Stock:"<<stock<<endl;
+}
+
+int main(){
+    Product p1;
+    Product p2;
+    p1.inputDetail();
+    p1.showDetail();
+    p2.inputDetail();
+    p2.showDetail();
+}
+
+/*
+Sample input
+
+P101 Laptop 55000 10
+P102 Mobile 25000 25
+
+*/
